fe703108d1a5f35d290cce5c4a253923436fed11
Yathwi <kaitoman.r.3@gmail.com>
2022-05-11 14:35:44 +0900

    test

--- src/component/aboutCompany.tsx
+++ src/component/aboutCompany.tsx
@@ -9,7 +9,7 @@ export default function AboutCompany(){
           <p>Miuit(ミウイット)は、「それ、使ってもいいですか？」という言葉を英語にした” May I use it ? ”が由来となっています。</p>
           <p>それは、私たちが既存のモノの価値を最大限発揮できるように支援することを基本理念としているからです。</p>
           <p>私たちのミッションは<span className=" text-yellow text-lg font-bold">Rethik Revalue</span>です。</p>
-          <p>遊休資産や過小評価されている場所・物・時間などあらゆるモノの価値の再定義を行っていきます。</p>
+          <p>遊休資産や過小評価されている場所・物・時間などあらゆるモノの価値の再定義を行っていきます<div className=""></div></p>
         </div>
       </div>
     </div>
